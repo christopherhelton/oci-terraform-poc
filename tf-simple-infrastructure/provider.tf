a82f942caffde0e74e273dc7ecd73db34c852502
Chris Helton <christopher.helton@humanit.us>
2023-10-30 15:59:42 -0400

    updated provider.tf

--- tf-simple-infrastructure/provider.tf
+++ tf-simple-infrastructure/provider.tf
@@ -1,7 +1,7 @@
 provider "oci" {
-  tenancy_ocid     = "ocid1.tenancy.oc1..aaaaaaaaunprf4yuhio6wulnx5tfb7do6hff3ztmbru6jws2tp7bf4deajgq"
-  user_ocid        = "chelton14@gmail.com"
-  private_key_path = "/home/oci/.oci/chelton.pem"
-  fingerprint      = "99:db:b7:7d:8d:89:dd:bf:34:d0:14:db:c9:c9:88:6d"
-  region           = "us-ashburn-1"
+  tenancy_ocid     = var.tenancy_ocid
+  user_ocid        = var.user_ocid
+  private_key_path = var.private_key_path
+  fingerprint      = var.fingerprint
+  region           = var.region
 }
